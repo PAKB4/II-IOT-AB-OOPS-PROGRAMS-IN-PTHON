eb494c138e43ee0ace86d7a60aa9659605dac118
PALADUGU ANIL KUMAR <anilkumarpaladugu401@gmail.com>
2025-10-11 10:55:30 +0530

    Add files via upload

---
+++ animal details.py
@@ -0,0 +1,27 @@
+class Animal:
+    def __init__(self,name):
+        self.name=name
+
+    def speak(self):
+        return"Generic animal sound"
+
+
+class Dog(Animal): #Dog inherits from Animal
+    def __init__(self,name,breed):
+        super().__init__(name)#call the parent class's constructor
+
+        self.breed=breed
+
+    def speak(self):
+        return "woof!"
+
+    def fetch(self):
+        return f"{self.name} is fetching the ball."
+#Create an instance of the child class (Dog)
+my_dog=Dog("Buddy","Golden Retriver")
+
+#Access attributes and methods from both parent and child classes
+print(f"Name:{my_dog.name}")
+print(f"Breed:{my_dog.breed}")
+print(my_dog.speak())
+print(my_dog.fetch())
